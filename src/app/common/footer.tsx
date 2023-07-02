c0a90eaaba087fdfdacbdc1ebe23869ec532f3a6
Karim Fawaz <karimhilalfawaz@gmail.com>
2023-07-02 13:54:28 +0100

    create navbar & footer and add contact image

--- src/app/common/footer.tsx
+++ src/app/common/footer.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
-
+import Image from 'next/image'
 export default function Footer() {
     return (
-        <footer>Footer</footer>
+        <footer className="flex flex-row justify-between p-4 h-50 items-center bg-[var(--dark-grey)] text-[var(--white)] fixed bottom-0 inset-x-0">
+            <p className="font-bold">Privacy Policy</p>
+            <Image alt="KAP Logo" src="/KAP-logo-white.svg" height={100} width={100} />
+            <div className="flex flex-row md:gap-8">
+                <p>About Us</p>
+                <p>Our Services</p>
+                <p>Contact Us</p>
+            </div>
+        </footer>
+
     )
 }
--- src/app/common/navbar.tsx
+++ src/app/common/navbar.tsx
@@ -1,7 +1,15 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function Navbar() {
     return (
-        <nav>navbar</nav>
+        <nav className="flex flex-row bg-[var(--white)] text-[var(--black)] justify-between p-4">
+            <Image alt="KAP Logo" src="/KAP-logo.svg" height={100} width={100} />
+            <div className="flex flex-row md:gap-8">
+                <p>About Us</p>
+                <p>Our Services</p>
+                <p>Contact Us</p>
+            </div>
+        </nav>
     )
 }
