f8fb47e8fc920e895decbaf963b56d3a4a882927
Karim Fawaz <karimhilalfawaz@gmail.com>
2023-09-02 14:51:56 +0300

    make footer responsive

--- src/app/common/footer.tsx
+++ src/app/common/footer.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link';
+
 export default function Footer() {
     return (
-        <footer className="flex flex-row justify-between p-4 h-50 items-center bg-[var(--dark-grey)] text-[var(--white)] ">
+        <footer className="flex flex-row justify-between p-4 h-50 items-center bg-[var(--dark-grey)] text-[var(--white)]">
             <p className="font-bold">Privacy Policy</p>
-            <Link href="/"><Image alt="KAP Logo" src="/KAP-logo-white.svg" height={100} width={100} /></Link>
-            <div className="flex flex-row md:gap-8">
+            <Link href="/" className="hidden md:flex">
+                <Image alt="KAP Logo" src="/KAP-logo-white.svg" height={100} width={100} />
+            </Link>
+            <div className="flex-row gap-8 hidden md:flex">
                 <p><Link href="/about">About Us</Link></p>
                 <p><Link href="/services">Our Services</Link></p>
                 <p><Link href="/contact">Contact Us</Link></p>
             </div>
         </footer>
-
     )
 }
