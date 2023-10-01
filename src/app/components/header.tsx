7a7557abbf6597b94f5b8629cdf1529568b2a318
Karim Fawaz <karimhilalfawaz@gmail.com>
2023-10-01 13:22:52 +0100

    fix main page responsiveness

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -4,8 +4,8 @@ import Events from './events'
 export default function Header() {
     return (
         <header className="bg-home text-[var(--white)]">
-            <div className='w-1/2 p-20'>
-                <h1 className="text-5xl font-bold">Your Partner in Growth.</h1>
+            <div className='md:w-1/2 md:p-20 p-10'>
+                <h1 className="text-2xl md:text-5xl font-bold">Your Partner in Growth.</h1>
                 <br />
                 <p className="text-sm">Kantar Advisory Partners "KAP" is a boutique consultancy specialising in supporting start-ups, businesses, and investors under our distinctive guiding principle of "Driving Impact, Powering Sustainability".</p>
             </div>
--- src/app/components/industry.tsx
+++ src/app/components/industry.tsx
@@ -7,19 +7,19 @@ export default function IndustryCoverage() {
     return (
         <section className="bg-[var(--dark-grey)] text-[var(--white)] backdrop-brightness-50 flex flex-col items-center p-8">
             <h1 className="text-4xl font-bold">Our Services</h1>
-            <br />
-            <br />
-            <div className="flex flex-wrap justify-center">
+
+            <div className="flex flex-wrap justify-center mt-12">
                 {coverage.map((service, index) => {
                     return (
-                        <div className="w-64 h-36 rounded-md outline outline-offset-2 outline-[var(--green)] flex justify-center items-center bg-black m-4"
+                        <div className="w-28 h-20 md:w-64 md:h-36 rounded-md outline outline-offset-2 outline-[var(--green)] flex justify-center items-center bg-black m-4"
 
                             key={index}>
-                            <p className="text-center text-lg">{service}</p>
+                            <p className="text-center md:text-lg text-xs">{service}</p>
                         </div>
                     );
                 })}
             </div>
+            <button className="mt-12 bg-[var(--green)] text-[var(--white)] rounded-lg p-3 w-64 font-bold">Find Out More</button>
 
         </section>
     )
