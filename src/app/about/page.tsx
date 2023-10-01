123bcf60d0217186aa3d01a77bc4781c591bedb3
Karim Fawaz <karimhilalfawaz@gmail.com>
2023-10-01 13:47:01 +0100

    fix responsiveness for about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -26,19 +26,18 @@ export default function About() {
 
     return (
         <div>
-            <div className="text-[var(--white)] text-center bg-about ">
-                <h1 className="md:text-6xl md:p-28 text-4xl font-bold p-20 backdrop-brightness-75">Unleash the untapped potential of your business.</h1>
-                <div className="backdrop-brightness-50 flex flex-col items-center p-16">
-                    <h3 className="font-bold text-4xl">Who We Are</h3>
+            <div className="text-[var(--white)] md:text-center bg-about">
+                <h1 className="md:text-6xl md:p-28 text-2xl font-bold p-6 backdrop-brightness-75 text-center">Unleash the untapped potential of your business.</h1>
+                <div className="backdrop-brightness-50 flex flex-col items-center md:p-16 p-6">
+                    <h3 className="font-bold md:text-4xl text-2xl">Who We Are</h3>
                     <br />
+                    <p className="md:w-3/4 text-xs p-6 md:p-0 md:text-lg">Welcome to Kantar Advisory Partners, where we unify sustainability and impactful investing under the guiding principle of “Driving Impact, Powering Sustainability.” We play the dual role of spotlighting strong, sustainable, and impactful investment opportunities that generate both impressive returns and positive impact, while also acting as a vital support for startups. More than 30 years of combined expertise in sustainability, impact investments, energy sector and event management, our team at KAP brings a vast network encompassing government entities, private sector stakeholders, investors, and local/regional start-ups in the cleantech, healthtech, agritech, and energy industries.</p>
                     <br />
-                    <p className="w-3/4">Welcome to Kantar Advisory Partners, where we unify sustainability and impactful investing under the guiding principle of “Driving Impact, Powering Sustainability.” We play the dual role of spotlighting strong, sustainable, and impactful investment opportunities that generate both impressive returns and positive impact, while also acting as a vital support for startups. More than 30 years of combined expertise in sustainability, impact investments, energy sector and event management, our team at KAP brings a vast network encompassing government entities, private sector stakeholders, investors, and local/regional start-ups in the cleantech, healthtech, agritech, and energy industries.</p>
-                    <br />
-                    <p className='font-bold'> At kap, we are not only service providers, but also partners on your journey towards sustainability.<br />Each step we take together is a stride towards a better, more sustainable future.</p>
+                    <p className='font-bold text-xs p-6 md:p-0 md:text-lg'> At kap, we are not only service providers, but also partners on your journey towards sustainability.<br /> <br />Each step we take together is a stride towards a better, more sustainable future.</p>
                 </div>
             </div>
-            <div className="bg-[var(--white)] text-[var(--green)] p-20">
-                <h1 className="text-2xl md:text-4xl">
+            <div className="bg-[var(--white)] text-[var(--green)] md:p-20 p-10 h-32 md:h-auto">
+                <h1 className="text-sm md:text-4xl">
                     <span className="font-bold ">We aspire to:</span>{" "}
                     {aspiration}
                 </h1>
