1e9c74755bcbd8714125807b13df634efc7b32a7
Karim Fawaz <karimhilalfawaz@gmail.com>
2023-11-01 20:41:44 +0000

    fix industry coverage

--- src/app/components/industry.tsx
+++ src/app/components/industry.tsx
@@ -5,20 +5,20 @@ export default function IndustryCoverage() {
         "Power, Renewable Energy & Utilities", "Hydrogen & Green Ammonia", "Sustainability & Planning Net Zero", "Water & Waste Management", "Transportation & Infrastructure", "CleanTech & AgriTech", "HealthTech", "Oil, Gas & LNG"
     ]
     return (
-        <section className="bg-[var(--dark-grey)] text-[var(--white)] backdrop-brightness-50 flex flex-col items-center p-8">
-            <h1 className="text-4xl font-bold">Industry Coverage</h1>
+        <section className="bg-[var(--dark-grey)] text-[var(--white)] backdrop-brightness-50 flex flex-col items-center md:p-8 p-4">
+            <h1 className="text-2xl md:text-4xl font-bold">Industry Coverage</h1>
 
-            <div className="flex flex-wrap justify-center mt-12">
+            <div className="flex flex-wrap justify-center md:mt-12 mt-4">
                 {coverage.map((coverageItem, index) => {
                     return (
-                        <div className="w-48 h-20 md:w-64 md:h-36 rounded-md outline outline-offset-2 outline-[var(--green)] flex justify-center items-center bg-black m-4"
+                        <div className="w-32 h-20 md:w-64 md:h-36 rounded-md outline outline-offset-2 outline-[var(--green)] flex justify-center items-center bg-black m-4"
                             key={index}>
-                            <p className="text-center md:text-xl text-md">{coverageItem}</p>
+                            <p className="text-center md:text-xl text-sm">{coverageItem}</p>
                         </div>
                     );
                 })}
             </div>
-            <button className="mt-12 bg-[var(--green)] text-[var(--white)] rounded-lg p-3 w-64 font-bold">Find Out More</button>
+            <button className="md:mt-12 mt-4 bg-[var(--green)] text-[var(--white)] rounded-lg p-3 w-64 font-bold">Find Out More</button>
 
         </section>
     )
