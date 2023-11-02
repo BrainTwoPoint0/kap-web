7613288bb26b2cb945e7a63b74fe8c6987097fe9
Karim Fawaz <karimhilalfawaz@gmail.com>
2023-11-02 00:15:34 +0000

    fix clients sizing and responsiveness

--- src/app/components/clients.tsx
+++ src/app/components/clients.tsx
@@ -2,21 +2,20 @@ import React from 'react'
 import Image from 'next/image'
 export default function Clients() {
     return (
-        <section className="bg-[var(--dark-grey)] p-16 text-[var(--green)] flex items-center flex-col">
-            <h2 className="text-4xl font-bold">Our Clients & Partners</h2>
-            <br />
-            <br />
-            <div className="flex flex-wrap space-x-4 justify-center items-center w-1/2">
-                <div className="h-24 w-24 relative"><Image alt="Arishi Logo" src="/arishi-logo.svg" fill /></div>
-                <div className="h-24 w-24 relative"><Image alt="ABCC Logo" src="/abcc-logo.svg" fill /></div>
-                <div className="h-24 w-24 relative"><Image alt="Civitem Logo" src=" /civitem-logo.svg" fill /></div>
-                <div className="h-24 w-24 relative"><Image alt="DKG Logo" src=" /dkg-logo.svg" fill /></div>
-                <div className="h-24 w-24 relative"><Image alt="FBBC Logo" src=" /fbbc-logo.svg" fill /></div>
-                <div className="h-24 w-24 relative"><Image alt="MBBC Logo" src=" /mbbc-logo.svg" fill /></div>
-                <div className="h-32 w-32 relative"><Image alt="Phyla Logo" src=" /phyla-logo.svg" fill /></div>
-                <div className="h-32 w-32 relative"><Image alt="Unitrove Logo" src=" /unitrove-logo.svg" fill /></div>
-                <div className="h-32 w-32 relative"><Image alt="Welkom Logo" src=" /welkom-logo.svg" fill /></div>
-                <div className="h-32 w-32 relative"><Image alt="ZEF Logo" src=" /zef-logo.svg" fill /></div>
+        <section className="bg-[var(--dark-grey)] p-8 md:p-16 text-[var(--green)] flex items-center flex-col">
+            <h2 className="text-2xl md:text-4xl font-bold md:mb-8">Our Clients & Partners</h2>
+
+            <div className="flex flex-wrap space-x-4 space-y-4 justify-center items-center md:w-1/2">
+                <img className="h-14 w-14 md:h-auto md:w-auto" alt="Arishi Logo" src="/arishi-logo.svg" />
+                <img className="h-14 w-14 md:h-auto md:w-auto" alt="ABCC Logo" src="/abcc-logo.svg" />
+                <img className="h-14 w-14 md:h-auto md:w-auto" alt="Civitem Logo" src=" /civitem-logo.svg" />
+                <img className="h-14 w-14 md:h-auto md:w-auto" alt="DKG Logo" src=" /dkg-logo.svg" />
+                <img className="h-14 w-14 md:h-auto md:w-auto" alt="FBBC Logo" src=" /fbbc-logo.svg" />
+                <img className="h-14 w-14 md:h-auto md:w-auto" alt="MBBC Logo" src=" /mbbc-logo.svg" />
+                <img className="h-14 w-14 md:h-auto md:w-auto" alt="Phyla Logo" src=" /phyla-logo.svg" />
+                <img className="h-14 w-14 md:h-auto md:w-auto" alt="Unitrove Logo" src=" /unitrove-logo.svg" />
+                <img className="h-14 w-14 md:h-auto md:w-auto" alt="Welkom Logo" src=" /welkom-logo.svg" />
+                <img className="h-14 w-14 md:h-auto md:w-auto" alt="ZEF Logo" src=" /zef-logo.svg" />
             </div >
         </section >
     )
