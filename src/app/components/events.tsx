ee8291efcd575a5ff5b4ef60fffc616320352291
Karim Fawaz <karimhilalfawaz@gmail.com>
2023-11-09 00:09:12 +0000

    fix events responsiveness

--- src/app/components/events.tsx
+++ src/app/components/events.tsx
@@ -1,11 +1,12 @@
 "use client"
+import Image from 'next/image';
 import React, { useState } from 'react'
 export default function Events() {
 
     const events = [
-        { id: "1", location: "London Olympia", date: "24-25 May 2023", description: "The International Climate & Clean Tech Congress", link: "https://www.innovationzero.com", image: "/innovationzero.png" },
-        { id: "2", location: "London Olympia", date: "24-25 May 2023", description: "The International Climate & Clean Tech Congress", link: "https://www.innovationzero.com", image: "/innovationzero.png" },
-        { id: "3", location: "London Olympia", date: "24-26 May 2023", description: "The International Climate & Clean Tech Congress", link: "https://www.innovationzero.com", image: "/innovationzero.png" },
+        { id: "1", location: "QEII Conference Centre", date: "6-7 November 2023", description: "The go-to-event for food security & sustainability", link: "https://sustainablefoodsevent.com", image: "/SFL.png" },
+        { id: "2", location: "Hilton London Metropole", date: "30 November 2023", description: "3rd Arab British Economic Summit", link: "https://abcc.glueup.com/event/68972/register/", image: "/arabbritishsummit.jpeg" },
+        { id: "3", location: "London Olympia", date: "24-25 May 2024", description: "The International Climate & Clean Tech Congress", link: "https://www.innovationzero.com", image: "/innovationzerologo.png" },
     ]
 
     const [activeIndex, setActiveIndex] = React.useState(0);
@@ -28,12 +29,12 @@ export default function Events() {
                         &lt;
                     </button>
 
-                    <a href={events[activeIndex].link} target='_blank' rel="noreferrer" className="bg-[var(--white)] transition-all duration-300 w-64 h-36 rounded-md flex ">
-                        <img src={events[activeIndex].image} alt="event" />
-                        <div className="text-black flex flex-col w-1/2 p-2 justify-center">
-                            <p className="font-semibold text-xs">{events[activeIndex].date}</p>
-                            <p className="font-thin text-xs">{events[activeIndex].location}</p>
-                            <p className="font-thin text-xs my-2.5">{events[activeIndex].description}</p>
+                    <a href={events[activeIndex].link} target='_blank' rel="noreferrer" className="bg-[var(--white)] transition-all duration-300 w-72 h-36 rounded-md flex">
+                        <Image src={events[activeIndex].image} width={140} height={150} alt="event" className="rounded-l-md border-r-black border-r-[1px]" />
+                        <div className="text-black flex flex-col w-1/2 p-2 mt-4 justify-start text-xs ">
+                            <p className="font-semibold ">{events[activeIndex].date}</p>
+                            <p className="font-thin text-[10px]">{events[activeIndex].location}</p>
+                            <p className="font-thin  my-2.5">{events[activeIndex].description}</p>
                         </div>
                     </a>
 
@@ -43,16 +44,16 @@ export default function Events() {
                 </div>
                 {events.map((event, index) => {
                     return (
-                        <a href={events[activeIndex].link} target='_blank' rel="noreferrer" className="bg-[var(--white)] w-96 h-52 rounded-md items-cente m-4 hidden md:flex"
+                        <div className="bg-[var(--white)] w-96 h-44 rounded-md items-cente m-4 hidden md:flex"
                             key={index}>
-                            <img src={event.image} alt={event.image} />
-                            <div className="text-black flex flex-col w-1/2 p-4">
-                                <p className="font-semibold text-lg">{event.date}</p>
+                            <Image src={event.image} alt={event.image} width={150} height={170} className="rounded-l-md border-r-black border-r-[1px]" />
+                            <div className="text-black flex flex-col p-4 text-sm w-1/2">
+                                <p className="font-semibold text-base">{event.date}</p>
                                 <p className="font-thin">{event.location}</p>
                                 <p className="font-thin my-3">{event.description}</p>
-                                <a href={event.link} target="_blank" rel="noreferrer" className="text-[var(--green)] underline text-xs">{event.link}</a>
+                                <a href={event.link} target="_blank" rel="noreferrer" className="text-[var(--green)] underline font-bold text-sm">Link</a>
                             </div>
-                        </a>
+                        </div>
                     );
                 })}
             </div>
