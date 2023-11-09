4f05d25e260908fdc9fa3155f6c67f1ceab524be
Karim Fawaz <karimhilalfawaz@gmail.com>
2023-11-09 00:11:15 +0000

    fix double quotes

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
             <div className='md:w-1/2 md:p-20 p-10'>
                 <h1 className="text-2xl md:text-5xl font-bold">Your Partner in Growth.</h1>
                 <br />
-                <p className="text-sm">Kantar Advisory Partners "KAP" is a boutique consultancy specialising in supporting start-ups, businesses, and investors under our distinctive guiding principle of "Driving Impact, Powering Sustainability".</p>
+                <p className="text-sm">Kantar Advisory Partners &quot;KAP&quot; is a boutique consultancy specialising in supporting start-ups, businesses, and investors under our distinctive guiding principle of "Driving Impact, Powering Sustainability".</p>
             </div>
             <Events />
         </header>
