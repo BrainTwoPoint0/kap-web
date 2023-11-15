a795849479289a17995524ba9b12d1315c792ffe
Karim Fawaz <karimhilalfawaz@gmail.com>
2023-11-15 16:02:15 +0000

    switch to use Next Image

--- src/app/components/clients.tsx
+++ src/app/components/clients.tsx
@@ -6,24 +6,24 @@ export default function Clients() {
             <h2 className="text-2xl md:text-4xl font-bold md:mb-8">Our Clients & Partners</h2>
 
             <div className="flex flex-wrap space-x-4 space-y-4 justify-center items-center md:w-1/2">
-                <img className="h-14 w-14 md:h-20 md:w-20" alt="Arishi Logo" src="/arishi-logo.svg" />
-                <img className="h-14 w-14 md:h-20 md:w-20" alt="ABCC Logo" src="/abcc-logo.svg" />
-                <img className="h-14 w-14 md:h-20 md:w-20" alt="Civitem Logo" src=" /civitem-logo.svg" />
-                <img className="h-14 w-14 md:h-20 md:w-20" alt="DKG Logo" src=" /dkg-logo.svg" />
-                <img className="h-14 w-14 md:h-20 md:w-20" alt="FBBC Logo" src=" /fbbc-logo.svg" />
-                <img className="h-14 w-14 md:h-20 md:w-20" alt="MBBC Logo" src=" /mbbc-logo.svg" />
-                <img className="h-14 w-14 md:h-20 md:w-20" alt="Phyla Logo" src=" /phyla-logo.svg" />
-                <img className="h-14 w-14 md:h-20 md:w-20" alt="Unitrove Logo" src=" /unitrove-logo.svg" />
-                <img className="h-14 w-14 md:h-20 md:w-20" alt="Welkom Logo" src=" /welkom-logo.svg" />
-                <img className="h-14 w-14 md:h-20 md:w-20" alt="ZEF Logo" src=" /zef-logo.svg" />
-                <img className="h-10 w-14 md:h-20 md:w-20" alt="aspen Logo" src=" /aspen-institute-logo-primary.png" />
-                <img className="h-10 w-16 md:h-14 md:w-24" alt="BE Logo" src=" /BE-logo.png" />
-                <img className="h-10 w-20 md:h-11 md:w-24" alt="Cambridge Cleantech Logo" src=" /Cambridge-Cleantech-logo.png" />
-                <img className="h-14 w-14 md:h-20 md:w-20" alt="OXFORD CLEANTECH Logo" src=" /OXFORD-CLEANTECH-LOGO.png" />
-                <img className="h-10 w-16 md:h-14 md:w-24" alt="SEC Logo" src=" /SEC-Logo.png" />
-                <img className="h-10 w-16 md:h-14 md:w-24" alt="Sustainable Logo" src=" /sustainable.png" />
-                <img className="h-10 w-16 md:h-14 md:w-24" alt="Kezad Logo" src=" /kezad.png" />
-                <img className="h-10 w-16 md:h-14 md:w-14" alt="IE Logo" src=" /improvedenergy.png" />
+                <Image className="h-14 w-14 md:h-20 md:w-20" width={100} height={100} alt="Arishi Logo" src="/arishi-logo.svg" />
+                <Image className="h-14 w-14 md:h-20 md:w-20" width={100} height={100} alt="ABCC Logo" src="/abcc-logo.svg" />
+                <Image className="h-14 w-14 md:h-20 md:w-20" width={100} height={100} alt="Civitem Logo" src="/civitem-logo.svg" />
+                <Image className="h-14 w-14 md:h-20 md:w-20" width={100} height={100} alt="DKG Logo" src="/dkg-logo.svg" />
+                <Image className="h-14 w-14 md:h-20 md:w-20" width={100} height={100} alt="FBBC Logo" src="/fbbc-logo.svg" />
+                <Image className="h-14 w-14 md:h-20 md:w-20" width={100} height={100} alt="MBBC Logo" src="/mbbc-logo.svg" />
+                <Image className="h-14 w-14 md:h-20 md:w-20" width={100} height={100} alt="Phyla Logo" src="/phyla-logo.svg" />
+                <Image className="h-14 w-14 md:h-20 md:w-20" width={100} height={100} alt="Unitrove Logo" src="/unitrove-logo.svg" />
+                <Image className="h-14 w-14 md:h-20 md:w-20" width={100} height={100} alt="Welkom Logo" src="/welkom-logo.svg" />
+                <Image className="h-14 w-14 md:h-20 md:w-20" width={100} height={100} alt="ZEF Logo" src="/zef-logo.svg" />
+                <Image className="h-10 w-14 md:h-20 md:w-20" width={100} height={100} alt="aspen Logo" src="/aspen-institute-logo-primary.png" />
+                <Image className="h-10 w-16 md:h-14 md:w-24" width={100} height={100} alt="BE Logo" src="/BE-logo.png" />
+                <Image className="h-10 w-20 md:h-11 md:w-24" width={100} height={100} alt="Cambridge Cleantech Logo" src="/Cambridge-Cleantech-logo.png" />
+                <Image className="h-14 w-14 md:h-20 md:w-20" width={100} height={100} alt="OXFORD CLEANTECH Logo" src="/OXFORD-CLEANTECH-LOGO.png" />
+                <Image className="h-10 w-16 md:h-14 md:w-24" width={100} height={100} alt="SEC Logo" src="/SEC-Logo.png" />
+                <Image className="h-10 w-16 md:h-14 md:w-24" width={100} height={100} alt="Sustainable Logo" src="/sustainable.png" />
+                <Image className="h-10 w-16 md:h-14 md:w-24" width={100} height={100} alt="Kezad Logo" src="/kezad.png" />
+                <Image className="h-10 w-16 md:h-14 md:w-14" width={100} height={100} alt="IE Logo" src="/improvedenergy.png" />
 
             </div >
         </section >
